0f4c68aafcde0f352d4e7cccc3c1b3ba6b1b9948
Jorge Valdez <georch@gmail.com>
2019-05-04 12:40:51 -0500

    feat: update theme components

--- src/components/templates/article/index.js
+++ src/components/templates/article/index.js
@@ -11,57 +11,59 @@ import './style.scss';
 
 const ArticlePage = ({ data }) => {
   const { domain } = data.site.siteMetadata;
+  const info = data.markdownRemark;
+
   return (
     <Layout
       showFooterCta
-      postUrl={`${domain}${data.nodeArticle.path.alias}`}
-      postSlug={data.nodeArticle.path.alias}
-      postTitle={data.nodeArticle.title}
-      postDesc={data.nodeArticle.fields.markdownBody.childMarkdownRemark.excerpt}
-      postDate={data.nodeArticle.fields.created_formatted}
-      postImage={`${domain}${data.nodeArticle.relationships.field_image.relationships.field_media_image.localFile.childImageSharp.fluid.src}`}
+      postUrl={`${domain}${info.fields.slug}`}
+      postSlug={info.frontmatter.path}
+      postTitle={info.frontmatter.title}
+      postDesc={info.excerpt}
+      postDate={info.frontmatter.date}
+      postImage={`${domain}${info.frontmatter.image.childImageSharp.fluid.src}`}
       isBlogPost
     >
       <Hero
-        title={data.nodeArticle.title}
-        image={data.nodeArticle.relationships.field_image.relationships.field_media_image.localFile.childImageSharp.fluid}
-        color={data.nodeArticle.field_overlay_color}
-        info={`By ${data.allSiteSettingEntitySite.edges[0].node.field_name} ● ${data.allSiteSettingEntitySite.edges[0].node.field_slogan} | ${data.nodeArticle.fields.created_formatted}`}
+        title={info.frontmatter.title}
+        image={info.frontmatter.image.childImageSharp.fluid}
+        color={info.frontmatter.overlay_color}
+        info={`By ${info.frontmatter.author.name} ● ${data.site.siteMetadata.settings.field_slogan} | ${info.frontmatter.date}`}
       />
       <div className="c-article u-push-top--inside--4x u-push-bottom--inside--4x">
         <div className="grid-container align-center grid-x">
           <div className="c-article__content cell small-11 medium-12 large-10 xlarge-10">
-            <div className="c-article__content" dangerouslySetInnerHTML={{ __html: data.nodeArticle.fields.markdownBody.childMarkdownRemark.html }} />
+            <div className="c-article__content" dangerouslySetInnerHTML={{ __html: info.html }} />
             <div className="c-article__tags">
               {
-              (!data.nodeArticle.relationships.field_tags) ? null
-                : data.nodeArticle.relationships.field_tags.map((tag, index) => (
-                  <span key={tag.path.alias}>
-                    <Link key={tag.path.alias} to={`/tags${tag.path.alias}`}>
-                      {`${tag.name}`}
+              (!info.frontmatter.tags) ? null
+                : info.frontmatter.tags.map((tag, index) => (
+                  <span key={tag.url}>
+                    <Link key={tag.url} to={`/tags${tag.url}`}>
+                      {`${tag.id}`}
                     </Link>
-                    {(index + 1 !== data.nodeArticle.relationships.field_tags.length) ? <span>,&nbsp;</span> : ''}
+                    {(index + 1 !== info.frontmatter.tags.length) ? <span>,&nbsp;</span> : ''}
                   </span>
                 ))
             }
             </div>
-            <Share shareUrl={`${domain}${data.nodeArticle.path.alias}`} shareTitle={data.nodeArticle.title} sharehandler={data.allSiteSettingEntitySite.edges[0].node.field_twitter_handle} />
+            <Share shareUrl={`${domain}${info.fields.slug}`} shareTitle={info.frontmatter.title} sharehandler={data.site.siteMetadata.settings.field_twitter_handle} />
           </div>
           <div className="cell small-11 medium-12 large-12">
-            {!data.nodeArticle.relationships.field_related_post ? null : (
+            {/* {!data.markdownRemark.relationships.field_related_post ? null : (
               <div className="c-article__related-post">
                 <div className="">
                   <h3>Related Posts</h3>
                   <div className="grid-x grid-margin-x">
-                    {data.nodeArticle.relationships.field_related_post.map(node => (
+                    {data.markdownRemark.relationships.field_related_post.map(node => (
                       <div className="cell small-6 medium-6 large-4 u-push-bottom" key={node.id}>
                         <ArticleTeaser
-                          title={node.title}
-                          image={node.relationships.field_image.relationships.field_media_image.localFile.childImageSharp.fluid}
-                          resume={node.field_resume}
-                          excerpt={node.fields.markdownBody.childMarkdownRemark.excerpt}
-                          link={node.path.alias}
-                          date={node.fields.created_formatted}
+                          title={node.frontmatter .title}
+                          image={node.frontmatter.image.childImageSharp.fluid}
+                          resume={node.frontmatter.resume}
+                          excerpt={node.excerpt}
+                          link={node.frontmatter.path}
+                          date={node.frontmatter.date}
                         />
                       </div>
                     ))}
@@ -69,11 +71,11 @@ const ArticlePage = ({ data }) => {
                 </div>
               </div>
             )
-        }
+        } */}
             <div className="">
               <Disqus
-                articleId={`${domain}${data.nodeArticle.path.alias}`}
-                title={data.nodeArticle.title}
+                articleId={`${domain}${info.fields.slug}`}
+                title={info.frontmatter.title}
               />
             </div>
           </div>
@@ -89,86 +91,42 @@ export const query = graphql`
     site{
       siteMetadata{
         domain
-      }
-    }
-    allSiteSettingEntitySite {
-      edges {
-        node {
+        settings {
           field_name
           field_slogan
           field_twitter_handle
         }
       }
     }
-    nodeArticle(fields: { slug: { eq: $slug } }) {
-      title
-      path {
-        alias
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      excerpt
+      frontmatter {
+        title
+        resume
+        date
+        overlay_color
+        author {
+          id
+          name
+          bio
+          twitter
+          website
+        }
+        tags {
+          id
+          url
+        }
+        image {
+          childImageSharp {
+            fluid {
+              src
+            }
+          }
+        }
       }
-      field_overlay_color
       fields {
         slug
-        created_formatted
-        markdownBody {
-          childMarkdownRemark {
-            html
-            rawMarkdownBody
-            excerpt
-          }
-        }
-      }
-      relationships {
-        field_tags{
-          name
-          path {
-            alias
-          }
-        }
-        field_image {
-          relationships {
-            field_media_image {
-              localFile {
-                publicURL
-                childImageSharp {
-                  fluid(maxWidth: 1440, maxHeight: 560, cropFocus: CENTER) {
-                    ...GatsbyImageSharpFluid
-                  }
-                }
-              }
-            }
-          }
-        }
-        field_related_post {
-          id
-          title
-          field_resume
-          path {
-            alias
-          }
-          fields {
-            created_formatted
-            markdownBody {
-              childMarkdownRemark {
-                excerpt
-              }
-            }
-          }
-          relationships {
-            field_image {
-              relationships {
-                field_media_image {
-                  localFile {
-                    childImageSharp {
-                      fluid(maxWidth: 600, maxHeight: 400, cropFocus: CENTER) {
-                        ...GatsbyImageSharpFluid
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
       }
     }
   }
